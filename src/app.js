a1900aad8208ddc5165467033a9970e801d9e47c
Sushant <sushantsingh408sks@gmail.com>
2025-01-04 13:36:24 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,4 +15,8 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({limit:"16kb"}));
 app.use(express.static("public"));
 
+import userRoutes from "./routes/user.routes.js";
+
+app.use("/api/v1/users" , userRoutes);
+
 export default app;
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
-        Promises.resolve(requestHandler(req, res, next)).catch( (err) => next(err) );
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch( (err) => next(err) );
     }
 }
 
