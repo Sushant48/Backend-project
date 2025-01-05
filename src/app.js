7a0695dd9339a09325c94913a8e01f8de36b4d82
Sushant <sushantsingh408sks@gmail.com>
2025-01-05 18:03:20 +0530

    Logic for register user

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({limit:"16kb"}));
 app.use(express.static("public"));
 
+
 import userRoutes from "./routes/user.routes.js";
 
 app.use("/api/v1/users" , userRoutes);
